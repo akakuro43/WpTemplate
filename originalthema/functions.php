31b12b023ee26248c2507f043c1593c6cd646178
遠藤雅俊 <ac.akakuro43@gmail.com>
2019-07-30 22:39:37 +0900

    プラグイン追加

--- originalthema/functions.php
+++ originalthema/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 function includePhpFunc($params = array()) {
   extract(shortcode_atts(array(
     'file' => 'default'
@@ -9,12 +10,74 @@ function includePhpFunc($params = array()) {
 }
 add_shortcode("includePhp","includePhpFunc");
 
+add_theme_support('post-thumbnails');
+
 global $wp_rewrite;
 $wp_rewrite->flush_rules();
 
-// function my_acf_init() {
-//   acf_update_setting('google_api_key', 'AIzaSyAmJYtfnH4UjxzYHbf724dR9aJeZ1bh1Ow');
-// }
-// add_action('acf/init', 'my_acf_init');
+// meta
+function echo_meta($name){
+  global $post;
+
+  switch($name){
+    case 'title':
+      if(is_home()){
+        $title = get_bloginfo('name');
+      }else{
+        $title = get_post_meta($post->ID, 'meta_title', true);
+
+        if(empty($title)){
+          $title = wp_title('', false);
+        }
+
+        $title .= ' | ' . get_bloginfo('name');
+      }
+
+      echo $title;
+
+      break;
+    case 'description':
+      if(is_home()){
+        $description = get_bloginfo('description');
+      }else if(get_post_type() == 'project' && is_archive()){
+        $description ="アーカイブ記事のデスクリプション";
+      }else if(get_post_type() == 'news' && is_archive()){
+        $description ="アーカイブ記事のデスクリプション";
+      }else if(is_single()){
+        $description = get_post_meta($post->ID, 'meta_title', true);
+      }else{
+        $description = get_post_meta($post->ID, 'meta_description', true);
+
+        if(empty($description)){
+          $description = get_bloginfo('description');
+        }
+      }
+
+      echo $description;
+
+      break;
+    case 'og_url':
+      if(is_home()){
+        $og_url = 'https://www.don-guri.com/';
+      }else{
+        $og_url = get_permalink($post->id);
+      }
+
+      echo $og_url;
+
+      break;
+    case 'og_image':
+      if(get_post_type() == 'project' && is_single()){
+        $og_image = the_post_thumbnail_url('full');
+
+      }else{
+        $og_image = bloginfo('template_url').'/assets/images/og_image.png';
+      }
+
+      echo $og_image;
+
+      break;
+  }
+}
 
 ?>
