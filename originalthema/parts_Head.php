fc6fdefb9b9ddafc4e39f100b086f8d36475a581
MasatoshiEndo <ac.akakuro43@gmail.com>
2019-07-31 13:53:53 +0900

    add Plugin

--- originalthema/parts_Head.php
+++ originalthema/parts_Head.php
@@ -1,48 +1,32 @@
-<?php
-  // 共通設定
-  $site_name = get_bloginfo('name');
-  $domain = "http://wp-original:8888/";
-  $url = parse_url( $_SERVER["REQUEST_URI"] );
-  $url = $url['path'];
-  $meta_description_base = get_bloginfo('description');
-  $meta_keywords_base = "";
-
-  // タイトル設定
-  global $meta_title;
-  if( !isset($meta_title) && get_the_title() && get_the_title() !== '' && !is_archive()){
-    $meta_title = get_the_title();
-  }
-  $meta_title = isset($meta_title) && $meta_title !== "" ? $meta_title . ' - ' . $site_name : $site_name;
-
-  //ディスクリプション設定
-  global $meta_description;
-  if( get_the_ID() && get_field('meta-description', get_the_ID() ) && get_field('meta-description', get_the_ID() ) !== '' ){
-    $meta_description = get_field('meta-description', get_the_ID());
-  }
-  $meta_description = isset($meta_description) ? $meta_description : $meta_description_base;
-
-  // キーワード設定
-  global $meta_keywords;
-  $meta_keywords = isset($meta_keywords) ? $meta_keywords : $meta_keywords_base;
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="Content-Language" content="ja">
-  <title><?php echo $meta_title; ?></title>
-  <meta name="description" content="<?php echo $meta_description; ?>">
-  <meta name="keywords" content="<?php echo $meta_keywords; ?>">
-  <meta property="og:title" content="<?php echo $meta_title; ?>">
+  <title><?php echo_meta('title'); ?></title>
+  <meta name="description" content="<?php echo_meta('description'); ?>">
+  <meta property="og:title" content="<?php echo_meta('title'); ?>">
   <meta property="og:type" content="website">
-  <meta property="og:url" content="https://<?php echo $domain; ?><?php echo $url; ?>">
-  <meta property="og:description" content="<?php echo $meta_description; ?>">
-  <meta property="og:site_name" content="<? echo $site_name; ?>">
+  <meta property="og:url" content="<?php echo_meta('og_url'); ?>">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="<?php echo_meta('description'); ?>">
   <meta property="og:locale" content="ja_JP">
-  <meta property="og:image" content="<?php bloginfo('template_url'); ?>/screenshot.png">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0, shrink-to-fit=no">
+  <meta property="og:image" content="<?php echo_meta('og_image'); ?>">
+  <meta property="fb:app_id" content="146752389058128">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="<?php echo_meta('title'); ?>">
+  <meta name="twitter:description" content="<?php echo_meta('description'); ?>">
+  <meta name="twitter:image" content="<?php echo_meta('og_image'); ?>">
+  <meta name="twitter:url" content="<?php echo_meta('og_url'); ?>">
   <meta name="format-detection" content="telephone=no">
+  <meta name="robots" content="<?php if(!get_field('noindex')): ?>all<?php else: ?>none<?php endif; ?>">
+  <link class="favicon" rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/assets/images/favicon.ico">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0, shrink-to-fit=no">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/common.css">
-  <?php wp_head(); ?>
+  <!-- Global site tag (gtag.js) - Google Analytics-->
+  <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-hogehoge-1"></script>
+  <script>window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-hogehoge-1');</script>
 </head>
